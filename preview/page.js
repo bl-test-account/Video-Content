a98f8cf625528320da2b6e40505cabcfa315f271
bl-test-account <plugin.developer@bytelight.com>
2014-05-13 20:19:15 -0700

    Released on: 2014-05-13 23:19:03 -0400

---
+++ preview/page.js
@@ -0,0 +1,17 @@
+/*
+Get the current location and user information by calling
+BL.getContext.The returned object contains the lightGroupId,
+lightGroupName, and userId properties.
+*/
+context = BL.getContext();
+ /*
+Retrieve static content with Bl.getStaticContent.
+Static content is stored as an object with properties
+equal to the identifiers of each piece of content. Each property
+contains an array with the content corresponding
+to each update of the app.
+*/
+var savedContent = BL.getStaticContent(APP.appId);
+var src = savedContent.video_url[0];
+$('video source')[0].src = src; //('src', src)
+alert($('video source')[0].src)
