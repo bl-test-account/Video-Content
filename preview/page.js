62b23a82cecc73dafe35589e27cc61b948151b4f
bl-test-account <plugin.developer@bytelight.com>
2014-05-13 20:59:49 -0700

    Released on: 2014-05-13 23:59:46 -0400

--- preview/page.js
+++ preview/page.js
@@ -12,11 +12,16 @@ contains an array with the content corresponding
 to each update of the app.
 */
 var savedContent = BL.getStaticContent(APP.appId);
+
+
+  vid = videojs('videoContent');
+  vid.src(savedContent.video_url[0]);
+  cosole.log(vid);
+  cosole.log(savedContent.video_url[0]);
+
 $('video').on('click', function() {
 //  alert(savedContent.video_url[0])
-  vid = videojs('videoContent')
-  vid.src(savedContent.video_url[0]);
 //  $('video source')[0].src = savedContent.video_url[0]; //('src', src)
-  vid.play()
+  $('video')[0].play()
 });
 //alert($('video source')[0].src)
