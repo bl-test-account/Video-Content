e6d67d7f5ab2a9d0269aa1aef08c9f6e5da82c67
bl-test-account <plugin.developer@bytelight.com>
2014-05-13 22:03:14 -0700

    Released on: 2014-05-14 01:03:12 -0400

--- preview/page.js
+++ preview/page.js
@@ -1,27 +1,16 @@
-/*
-Get the current location and user information by calling
-BL.getContext.The returned object contains the lightGroupId,
-lightGroupName, and userId properties.
-*/
-context = BL.getContext();
- /*
-Retrieve static content with Bl.getStaticContent.
-Static content is stored as an object with properties
-equal to the identifiers of each piece of content. Each property
-contains an array with the content corresponding
-to each update of the app.
-*/
-var savedContent = BL.getStaticContent(APP.appId);
+//context = BL.getContext();
+
+//var savedContent = BL.getStaticContent(APP.appId);
 
 
-  vid = videojs('videoContent');
-  vid.src(savedContent.video_url[0]);
-  cosole.log(vid);
-  cosole.log(savedContent.video_url[0]);
+//  vid = videojs('videoContent');
+//  vid.src(savedContent.video_url[0]);
+//  cosole.log(vid);
+//  cosole.log(savedContent.video_url[0]);
 
-$('video').on('click', function() {
-//  alert(savedContent.video_url[0])
-//  $('video source')[0].src = savedContent.video_url[0]; //('src', src)
-  $('video')[0].play()
-});
+//$('video').on('click', function() {
+////  alert(savedContent.video_url[0])
+////  $('video source')[0].src = savedContent.video_url[0]; //('src', src)
+//  $('video')[0].play()
+//});
 //alert($('video source')[0].src)
